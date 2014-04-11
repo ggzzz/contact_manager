cc42113109cea729d0c858f0371cdb4a62ce8f03
b <a>
2014-04-11 22:06:52 +0800

    implement PhoneNumber and its association with Person

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe PhoneNumber do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:phone_number) {PhoneNumber.new(number: '329423478', person_id: 1)}
+
+  it 'is valid' do
+  	expect(phone_number).to be_valid
+  end
+
+  it 'is invalid without a number' do
+  	phone_number.number = nil
+  	expect(phone_number).not_to be_valid
+  end
+
+  it 'must have a reference to person' do
+  	phone_number.person_id = nil
+  	expect(phone_number).not_to be_valid
+  end
+
+  it 'is associated with a person' do
+  	expect(phone_number).to respond_to(:person)
+  end
 end
